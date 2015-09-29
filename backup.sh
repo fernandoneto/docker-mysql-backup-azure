c96f2d6a2d55b7d9a273c2c7715c3f835132e8b5
Fernando <fernando.neto27051987@gmail.com>
2015-09-29 12:52:05 +0100

    change

--- backup.sh
+++ backup.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-. /backup/functions.sh
-
 if [ "$BACKUP_WINDOW" == "" ]; then
     BACKUP_WINDOW="0 6 * * * ";
 fi
 
 if  [ "$ONE_SHOOT" == "true" ]; then
-    make_backup;
-    exit 0;
+    . /backup/functions.sh;
+    exit 0
 else
     # scheduele backup window
-    crontab -l | { cat; echo "$BACKUP_WINDOW /backup/functions.sh"; } | crontab -
-
-    cron -f -L 8
-
+    crontab -l | { cat; echo "$BACKUP_WINDOW /backup/functions.sh"; } | crontab -;
+    cron -f -L 8;
     exit $?
-
 fi
