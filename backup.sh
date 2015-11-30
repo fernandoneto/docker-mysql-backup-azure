4739032f748bdb0512380a89d8673a71abdfbe77
joseneto <fernando.neto@findhit.com>
2015-11-30 18:49:01 +0000

    new aproach

--- backup.sh
+++ backup.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
 if [ "$BACKUP_WINDOW" == "" ]; then
+
     BACKUP_WINDOW="0 6 * * * ";
+
 fi
 
 if  [ "$ONE_SHOOT" == "true" ]; then
+
     . /backup/functions.sh;
     exit 0
+
 else
-    # scheduele backup window
+
+    sed 's,{{MYSQL_HOST}},'"${MYSQL_HOST}"',g' -i /backup/variable.sh
+    sed 's,{{MYSQL_PORT}},'"${MYSQL_PORT}"',g' -i /backup/variable.sh
+    sed 's,{{DB_USER}},'"${DB_USER}"',g' -i /backup/variable.sh
+    sed 's,{{DB_PASSWORD}},'"${DB_PASSWORD}"',g' -i /backup/variable.sh
+    sed 's,{{DB_NAME}},'"${DB_NAME}"',g' -i /backup/variable.sh
     touch /var/log/cron.log;
-    echo "$BACKUP_WINDOW /backup/functions.sh >> /var/log/cron.log 2>&1"\n >> job;
-    crontab job;
+    echo "$BACKUP_WINDOW /backup/variable.sh & /backup/functions.sh >> /var/log/cron.log 2>&1" >> job;
+    echo "" >> job
+    cron job;
     tail -f /var/log/cron.log;
     exit $?
+
 fi
---
+++ variable.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+export MYSQL_HOST={{MYSQL_HOST}}
+export MYSQL_PORT={{MYSQL_PORT}}
+export DB_USER={{DB_USER}}
+export DB_PASSWORD={{DB_PASSWORD}}
+export DB_NAME={{DB_NAME}}
